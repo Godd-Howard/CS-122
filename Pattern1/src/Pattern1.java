45d6378059e81529ffc888f13b2f5720fcafd8a4
Godd-Howard <57734377+Godd-Howard@users.noreply.github.com>
2020-08-04 22:49:14 -0400

    Add files via upload
    Spacing practice 'JAVA'

---
+++ Pattern1/src/Pattern1.java
@@ -0,0 +1,24 @@
+/**
+ * This program will print out Java in block letters using the same 
+ * letter as the block letter to make it.
+ * Made by Kurtis Yoder
+ */
+
+public class Pattern1 
+{
+    public static void main(String[] args)
+    {
+    	System.out.println(" JJJJJJJJJJJJJ        A        V             V  "+
+    	                   "      A");
+    	System.out.println("       J\t     A A        V\t    V  " +
+    	                   "      A A\n       J\t    A   A\t V\t   V" +
+    	                   "        A   A");
+    	System.out.print  ("       J\t   A     A\t  V       V\t   A     A\n");
+    	System.out.println("       J\t  AAAAAAAAA\t   V     V\t  AAAAAAAAA");
+    	System.out.println("J      J\t A\t   A\t    V   V        A\t   A");
+    	System.out.println(" J    J         A           A\t     V V" +
+    	                   "        A           A");
+    	System.out.println("  JJJJ         A             A        V" +
+    	                   "        A             A");
+    }
+}
